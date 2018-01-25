a6aabd2118402db15dfeb4c207fea88668973467
Juan Pablo Arevalo <juan.arevalo.merchan@hotmail.com>
2018-01-24 21:07:40 -0500

    Lab 1

---
+++ angular-seed/src/app/models/todo.ts
@@ -0,0 +1,12 @@
+import { TodoService } from '../services/todo.service';
+export class Todo {
+	private description: string;
+	private priority: Number;
+	private completed: boolean;
+
+	constructor(description: string, priority: Number = 1, completed: boolean = false) {
+		this.description = description;
+		this.completed = completed;
+		this.priority = priority;
+	}
+}
