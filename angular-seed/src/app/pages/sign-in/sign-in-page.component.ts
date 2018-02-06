4e97ec4e81cfea812492a6e257a88d31897ac5fb
Administrador <Administrador@192.168.5.1>
2018-02-06 17:04:52 -0500

    Arreglos varios

--- angular-seed/src/app/pages/sign-in/sign-in-page.component.ts
+++ angular-seed/src/app/pages/sign-in/sign-in-page.component.ts
@@ -2,16 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { UsersService } from '../../services/users.service';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AuthService } from '../../common/auth.service';
 
 @Component({
   selector: 'app-sign-in-page',
   templateUrl: './sign-in-page.component.html',
   styleUrls: ['./sign-in-page.component.css']
   })
+  
 export class SignInPageComponent implements OnInit {
-    public usersService: UsersService;
+    
     public signInForm: FormGroup;
-   constructor(public formBuilder: FormBuilder, public router: Router) {
+    public authService: AuthService;
+   constructor(public formBuilder: FormBuilder, public router: Router, public usersService: UsersService) {
     }
 
   ngOnInit() {
